cb7a9e24fe3352bbb6a78f9ece1cb7b9c7ba1334
Mojmira <mielke.magdalena99@gmail.com>
2021-02-03 19:12:25 +0100

    Pierwsze pliki

---
+++ Assets/Scripts/ClothController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothController : MonoBehaviour
+{
+
+    public Point[] linkA;
+    public Point[] linkB;
+    public float[] restDist;
+
+    public LineRenderer lineRenderer;
+     
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.black;
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+        lineRenderer.positionCount = 2;
+        lineRenderer.useWorldSpace = true;
+
+        for(int i = 0; i < linkA.Length; i++)
+        {
+            restDist[i] = linkA[i].InitializeLinks(linkB[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i<linkA.Length;i++)
+        {
+            linkA[i].SolveLink(linkB[i], restDist[i], lineRenderer);
+        }
+    }
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    static public float Stiffnes = 1f;
+    static public float Size = 0;
+
+    
+    private Vector3 newPos;
+    private Vector3 oldPos;
+
+    private Vector3 gravity = new Vector3(0f, -0.98f, 0f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = this.transform.position;
+        oldPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inertia();
+        this.transform.position += gravity * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        oldPos = newPos;
+        newPos = this.transform.position;
+    }
+
+
+    private void Inertia()
+    {
+        Vector3 vel = this.transform.position - oldPos;
+        newPos = this.transform.position + vel * Time.deltaTime;
+        this.transform.position = newPos;
+    }
+
+    public void SolveLink(Point pointB, float restD, LineRenderer lineRenderer)
+    {
+        Vector3 posA = this.transform.position;
+        Vector3 posB = pointB.transform.position;
+
+        float dist = Vector3.Distance(posA, posB);
+
+        float difference = (restD + Size - dist) / dist;
+
+        Vector3 translate = (posA - posB) * 0.5f * difference * Stiffnes;
+
+        this.transform.position += translate * Time.deltaTime;
+        pointB.transform.position -= translate * Time.deltaTime;
+
+        lineRenderer.SetPosition(0, this.transform.position);
+        lineRenderer.SetPosition(1, pointB.transform.position);
+    }
+
+    public float InitializeLinks(Point pointB)
+    {
+        Vector3 posA = this.transform.position;
+        Vector3 posB = pointB.transform.position;
+
+        float dist = Vector3.Distance(posA, posB);
+        return dist;
+    }
+
+
+}
