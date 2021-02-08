de8ec3ca967130b0a86f1f73a2d7f36e332666c5
ondrikk <fajnygo@yahoo.pl>
2021-02-08 19:54:35 +0100

    Added buttons to control animation

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PauseAnimation()
+    {
+        anim.speed = 0;
+    }
+
+    public void PlayAnimation()
+    {
+        anim.speed = 1;
+    }
+
+    public void StopAnimation()
+    {
+        // call mediator to reset model transform to initial
+        anim.Play("Idle", -1, 0f);
+        this.PauseAnimation();
+    }
+}
