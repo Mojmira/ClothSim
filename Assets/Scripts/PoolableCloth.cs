3f7b372550eb73b9fe2f89a62c1c624c19eae77e
ondrikk <fajnygo@yahoo.pl>
2021-02-09 23:41:52 +0100

    Fixed cloth selection
    Fixed cloth selection 2
    
    Fixed cloth selection 3
    
    Fixed cloth selection 4

--- Assets/Scripts/PoolableCloth.cs
+++ Assets/Scripts/PoolableCloth.cs
@@ -5,4 +5,47 @@ using UnityEngine;
 public class PoolableCloth : MonoBehaviour
 {
     public int number;
+    
+    public static Transform GetParentTransform(int number)
+    {
+        switch(number)
+        {
+            case 2: // attach hat to head
+                return GameObject.Find("m_avg_Head").transform;
+                break;
+            default: // attach cape and chiton to neck
+                return GameObject.Find("m_avg_Neck").transform;
+                break;
+        }
+    }
+
+    public void AttachColliders()
+    {
+        string[] collidersNames;
+
+        switch (number)
+        {
+            case 2: // hat colliders
+                collidersNames = new string[] { "m_avg_Neck", "m_avg_Head", "m_avg_L_Shoulder", "m_avg_R_Shoulder", "m_avg_Spine2"};
+                break;
+            case 1: // chiton colliders
+                collidersNames = new string[] { "m_avg_L_Collar", "m_avg_R_Collar", "m_avg_Spine1", "m_avg_Spine2", "m_avg_L_Hip", "m_avg_R_Hip", "m_avg_Pelvis" };
+                break;
+            case 0: // cape colliders
+                collidersNames = new string[] { "m_avg_L_Collar", "m_avg_R_Collar", "m_avg_Spine1", "m_avg_Spine2", "m_avg_Pelvis" };
+                break;
+            default:
+                collidersNames = null;
+                break;
+        }
+
+        CapsuleCollider[] capsuleColliders = new CapsuleCollider[collidersNames.Length];
+
+        for(int i = 0; i < collidersNames.Length; i++)
+        {
+            capsuleColliders[i] = GameObject.Find(collidersNames[i]).GetComponent<CapsuleCollider>();
+        }
+
+        GetComponent<Cloth>().capsuleColliders = capsuleColliders;
+    }
 }
