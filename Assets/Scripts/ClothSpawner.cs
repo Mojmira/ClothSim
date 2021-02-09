2ad20f21aa8cef373dba84f83f79561106ed54e9
ondrikk <fajnygo@yahoo.pl>
2021-02-09 13:41:43 +0100

    Added object pool

---
+++ Assets/Scripts/ClothSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private ClothPool clothPool;
+
+    private PoolableCloth lastSpawned;
+
+    public void Spawn(int number)
+    {
+        if (!lastSpawned || lastSpawned.number != number)
+        {
+            if (lastSpawned)
+                clothPool.ReturnToPool(lastSpawned);
+
+            PoolableCloth instance = clothPool.GetPrefabInstance(number);
+            instance.transform.position = transform.position; // set instance transform position to model position
+            lastSpawned = instance;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PoolableCloth.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolableCloth : MonoBehaviour
+{
+    public int number;
+}
