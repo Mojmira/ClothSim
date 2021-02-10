87be0ae303868890dbdede46a4a7b30b1893b8cf
Mojmira <mielke.magdalena99@gmail.com>
2021-02-10 04:10:29 +0100

    Added state pattern

--- Assets/Scripts/ClothSpawner.cs
+++ Assets/Scripts/ClothSpawner.cs
@@ -7,17 +7,14 @@ public class ClothSpawner : MonoBehaviour
     [SerializeField]
     private ClothPool clothPool;
 
-    private PoolableCloth lastSpawned;
+    public PoolableCloth lastSpawned;
 
-    [SerializeField]
-    private GUIMediator mediator; // type should be Mediator but then Unity wouldn't diplay it in inspector
 
     public void Spawn(int number)
     {
         if (!lastSpawned || lastSpawned.number != number)
         {
             RemoveLastSpawned();
-            mediator.notify("StopAnimation");
 
             PoolableCloth instance = clothPool.GetPrefabInstance(number);
             //instance.transform.position = transform.position; // set instance transform position to model position
@@ -35,4 +32,5 @@ public class ClothSpawner : MonoBehaviour
         }
 
     }
+
 }
---
+++ Assets/Scripts/IState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IState
+{
+    public ClothSpawner spawner;
+
+    
+
+    public  void ChangeDamping(float newDamp)
+    {
+        spawner.lastSpawned.GetComponent<Cloth>().damping = newDamp;
+    }
+    public abstract void ChangeSize(float newSize);
+
+}
